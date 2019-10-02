eae6e4584ecc08f599c5cfad022e778fbe764f5d
wlukla <vladislav.danilochkin@gmail.com>
2019-10-03 02:03:21 +0300

    feat: add solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,141 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        let tempNode = new Node(data);
 
-    head() {}
+        // for empty list
+        if (this._head == null) {
+            this._head = tempNode;
+            this._tail = tempNode;
+        }
+        // for non-empty list
+        else {
+            tempNode.prev = this._tail;
+            this._tail.next = tempNode;
+            this._tail = tempNode;
+        }
+        this.length++;
+        return this;
+    }
 
-    tail() {}
+    head() {
+        if (this.length != 0) {
+            return this._head.data;
+        }
+        else {
+            return this._head;
+        }
+    }
 
-    at(index) {}
+    tail() {
+        if (this.length != 0) {
+            return this._tail.data;
+        }
+        else {
+            return this._tail;
+        }
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        let counter = 0;
+        let current = this._head;
+        while (counter != index) {
+            current = current.next;
+            counter++;
+        }
+        return current.data;
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        let counter = 0;
+        let current = this._head;
+        let tempNode = new Node(data);
 
-    clear() {}
+        // for empty list with input index == 0;
+        if (this._head == null && index == 0) {
+            this._head = tempNode;
+            this.tail = tempNode;
+            this.length++;
+        }
+        // for non-empty list with index > 0;
+        else if (this._head != null && index > 0) {
+            while (counter != index) {
+                current = current.next;
+                counter++;
+            }
+            tempNode.prev = current.prev;
+            tempNode.next = current;
+            current.prev.next = tempNode;
+        }
+        return this;
+    }
 
-    deleteAt(index) {}
+    isEmpty() {
+        return this.length == 0;
+    }
 
-    reverse() {}
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
 
-    indexOf(data) {}
+    deleteAt(index) {
+        let counter = 0;
+        let current = this._head;
+        if (this._head == this._tail) {
+            this._head = null;
+            this._tail = null;
+        } else {
+            while (counter != index) {
+                current = current.next;
+                counter++;
+            }
+            current.next.prev = current.prev;
+            current.prev.next = current.next;
+        }
+        this.length--;
+        return this;
+    }
+
+    reverse() {
+        let current = this._head;
+        let prev = null;
+        while (current != null) {
+            let next = current.next;
+            // replacing prev and next
+            current.next = prev;
+            current.prev = next;
+            // updating prev
+            prev = current;
+            // using next to update current bc current.next was replased
+            current = next;
+        }
+        this._tail = this._head;
+        this._head = prev;
+        return this;
+    }
+
+    indexOf(data) {
+        let counter = 0;
+        let current = this._head;
+
+        for (let i = 0; i < this.length; i++) {
+            if (current.data == data) {
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
